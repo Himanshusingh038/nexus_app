a60406c8571ecabf62b63ff5ea127115f7221414
Jahanvi Jasani <jasanijahanvi@gmail.com>
2022-12-16 19:39:41 +0530

    dashboard statistics section completed

--- client_side/src/pages/dashboard.js
+++ client_side/src/pages/dashboard.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { Box, Container, Grid } from '@mui/material';
-import { HomeOutlined } from '@mui/icons-material';
+import { PeopleAltOutlined, CreditCardOutlined, CreditScoreOutlined, CreditCardOffOutlined, PersonAddAltOutlined, ContactPageOutlined } from '@mui/icons-material';
 import { LatestOrders } from '../components/dashboard/latest-orders';
 import { StatisticsCard } from '../components/dashboard/statistics-card';
 import { DashboardLayout } from '../components/dashboard-layout';
@@ -34,8 +34,8 @@ const Dashboard = () => (
             <StatisticsCard 
               title="Total Customers"
               value="170"
-              icon={HomeOutlined}
-              bg='#d14343'
+              icon={PeopleAltOutlined}
+              bg='#b19cd9'
             />
           </Grid>
           <Grid
@@ -48,8 +48,64 @@ const Dashboard = () => (
             <StatisticsCard 
               title="Total Cards"
               value="156"
-              icon={HomeOutlined}
-              bg='#14b8a6'
+              icon={CreditCardOutlined}
+              bg='#78a2cc'
+            />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <StatisticsCard 
+              title="Active Cards"
+              value="156"
+              icon={CreditScoreOutlined}
+              bg='#4e9c81'
+            />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <StatisticsCard 
+              title="Incomplete Cards"
+              value="73"
+              icon={ContactPageOutlined}
+              bg='#ffb836'
+            />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <StatisticsCard 
+              title="Unassigned Cards"
+              value="48"
+              icon={PersonAddAltOutlined}
+              bg='#e69690'
+            />
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            <StatisticsCard 
+              title="Inactive Cards"
+              value="2"
+              icon={CreditCardOffOutlined}
+              bg='#adaea5'
             />
           </Grid>
           <Grid
