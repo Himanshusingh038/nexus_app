0456ab980835c835db85af8145de68988b610734
himanshusingh038 <himanshusingh038@gmail.com>
2023-01-29 12:35:05 +0530

    backend_refactoring part1

--- server_side/index.js
+++ server_side/index.js
@@ -1,91 +1,85 @@
-// index.js
-
-/**
- * Required External Modules
- */
 const express = require("express");
-const path = require("path");
-const bodyParser = require('body-parser')
-const db = require('./queries')
-const pg = require('pg').Pool
-const session = require('express-session');
-const pgSession = require('connect-pg-simple')(session);
+const bodyParser = require("body-parser");
+const db = require("./queries");
+const pg = require("pg").Pool;
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
 const isAuth = require("./middleware/is-auth");
-const cors = require('cors');
-const { log } = require("console");
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const activateCards = require("./routes/activate_card");
+const getActiveCards = require("./routes/active_cards");
+const getInactiveCards = require("./routes/inactive_cards");
+const dashboardStats = require("./routes/dashboard_stats");
+const getIncompleteCards = require("./routes/incomplete_cards");
+const getCustomers = require("./routes/get_customers");
+const getCustomersById = require("./routes/get_customers_by_id");
+const checkCustomerExists = require("./routes/check_customer_exists");
+const updateRemark = require("./routes/update_remarks");
+const login = require("./routes/login");
+const logout = require("./routes/logout");
+const customerAction = require("./routes/customer_actions");
+const editCustomer = require("./routes/edit_customer");
+const getAllCards = require("./routes/all_cards");
+const updatePassword = require("./routes/update_password");
+const unassignedCardsAction = require("./routes/unassigned_action");
+const getUnassignedCard = require("./routes/get_unassigned_card");
+const generatePromotional = require("./routes/generate_promotional");
 
-
-/**
- * App Variables
- */
 const app = express();
 
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 const port = process.env.PORT || "8000";
 const pgPool = new pg({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'nexusDB',
-  password: 'pass@123',
+  user: "postgres",
+  host: "localhost",
+  database: "nexusDB",
+  password: "pass@123",
   port: 5432,
-})
+});
 
-/**
- *  App Configuration
- */
- app.use(bodyParser.json())
- app.use(
-   bodyParser.urlencoded({
-     extended: true,
-   })
- )
- app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  // store: new (require('connect-pg-simple')(session))(),
-  store: new pgSession({
-    pool : pgPool,                // Connection pool
-    tableName : 'sessions' ,// Use another table-name than the default "session" one
-    createTableIfMissing: true,  
-    // Insert connect-pg-simple options here
-  }),
-  secret: 'secret',
-  resave: false,
-  cookie: { maxAge: 4 * 60 * 60 * 1000 ,
-    'domain' : 'localhost'
-  } // 4 hours 
-  // Insert express-session options here
-}));
-/**
- * Routes Definitions
- */
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    store: new pgSession({
+      pool: pgPool,
+      tableName: "sessions",
+      createTableIfMissing: true,
+    }),
+    secret: "secret",
+    resave: false,
+    cookie: { maxAge: 4 * 60 * 60 * 1000, domain: "localhost" },
+  })
+);
 console.log(isAuth);
-app.post('/generate_promotional',isAuth,db.generatePromotional)
-app.post('/generate_existing',isAuth, db.generateExisting)
-app.get('/get_unassigned_card',isAuth,db.getUnassignedCard)
-app.get('/unassigned_action',isAuth, db.unassignedCardsAction)
-app.get('/active',isAuth, db.getActiveCards)
-app.get('/inactive',isAuth, db.getInactiveCards)
-app.get('/incompete', isAuth, db.getIncompleteCards)
-app.get('/get_customers',isAuth,  db.getCustomers)
-app.get('/get_customers/:cst_id', isAuth,  db.getCustomerById)
-app.get('/check_customer_exists',  db.checkCustomerExists)
-app.post('/activate_card',isAuth, db.activateCards)
-app.get('/dashboard_stats',isAuth, db.dashboardStats)
-app.post('/update_remarks',isAuth, db.updateRemark)
-app.post('/login', db.login)
-app.get('/logout', db.logout)
-app.get('/customer_actions', isAuth,db.customerAction)
-app.post('/edit_customer',isAuth, db.editCustomer)
-app.get('/all_cards', db.getAllCards)
-app.post('/update_password',db.updatePassword)
+app.post("/generate_promotional", isAuth, generatePromotional);
+app.post("/generate_existing", isAuth, db.generateExisting);
+app.get("/get_unassigned_card", isAuth, getUnassignedCard);
+app.get("/unassigned_action", isAuth, unassignedCardsAction);
+app.get("/active", isAuth, getActiveCards);
+app.get("/inactive", isAuth, getInactiveCards);
+app.get("/incompete", isAuth, getIncompleteCards);
+app.get("/get_customers", isAuth, getCustomers);
+app.get("/get_customers/:cst_id", isAuth, getCustomersById);
+app.get("/check_customer_exists", checkCustomerExists);
+app.post("/activate_card", isAuth, activateCards);
+app.get("/dashboard_stats", isAuth, dashboardStats);
+app.post("/update_remarks", isAuth, updateRemark);
+app.post("/login", login);
+app.get("/logout", logout);
+app.get("/customer_actions", isAuth, customerAction);
+app.post("/edit_customer", isAuth, editCustomer);
+app.get("/all_cards", getAllCards);
+app.post("/update_password", updatePassword);
 
-
-/**
- * Server Activation
- */
-app.listen(port,()=>{
-    console.log(`listining to the ${port} on node server`)
-})
+app.listen(port, () => {
+  console.log(`listining to the ${port} on node server`);
+});
