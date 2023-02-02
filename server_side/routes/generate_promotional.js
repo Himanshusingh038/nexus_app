7cbddf26c9d640783b504da123293ec2fdc0189c
himanshusingh038 <himanshusingh038@gmail.com>
2023-02-02 13:55:22 +0530

    card_id and card_no logic changed

--- server_side/routes/generate_promotional.js
+++ server_side/routes/generate_promotional.js
@@ -5,9 +5,11 @@ module.exports = (req, res) => {
   var status = "failed";
   var ans = [];
   while (card_count--) {
-    let card_num = Math.floor(Math.random() * 10000);
+    let card_num = Math.floor(new Date().getTime()/100).toString() + Math.floor(Math.random() *(10000)).toString();
     console.log(card_num);
-    sql = `INSERT INTO cards(card_unique_id,card_status) values(${card_num},'unassigned') returning *`;
+    const card_id = btoa(card_num);
+    sql = `INSERT INTO cards(card_id, card_unique_id,card_status) values('${card_id}',${card_num},'unassigned') returning *`;
+    console.log(sql);
     pool.query(sql, (err, results) => {
       if (err) {
         throw err;
